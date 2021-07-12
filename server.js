d84271889c843dd8379e1de0a88d7cbd573fc6ec
Mikeysoar <mwclimb@gmail.com>
2021-07-12 16:08:37 -0600

    20210712-1608

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const db = require('../u-develop-it/db/connection');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const port = process.env.PORT || 3001;
 const app = express();
 
 // middleware
-app.use(express
-app.use
-app.use
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-    console.log (`API server now on port ${PORT}!`);
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`API server now on port ${PORT}!`);
+    });
 });
